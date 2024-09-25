8084c5fe7c9c78aa206c808bf5204cc6247b5924
Jerucho <amadorjeremias2006@gmail.com>
2024-09-24 20:45:00 -0500

    agg: Menu superior y botones de acceso directo

--- SharpAED/src/gui/Home.java
+++ SharpAED/src/gui/Home.java
@@ -7,11 +7,29 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Home extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JMenuBar menuBar;
+	private JMenu mnNewMenu;
+	private JMenu mnNewMenu_1;
+	private JMenu mnNewMenu_2;
+	private JMenu mnNewMenu_3;
+	private JMenu mnNewMenu_4;
+	private JPanel panel;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	private JButton btnNewButton_5;
 
 	/**
 	 * Launch the application.
@@ -32,27 +50,58 @@ public class Home extends JDialog {
 	 * Create the dialog.
 	 */
 	public Home() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 823, 509);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+		contentPanel.setLayout(null);
+		
+		panel = new JPanel();
+		panel.setBounds(109, 71, 588, 306);
+		contentPanel.add(panel);
+		panel.setLayout(new GridLayout(0, 3, 10, 10));
+		
+		btnNewButton = new JButton("Registrar Venta");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionPerformedBtnNewButton(e);
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		});
+		panel.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("Agregar Cliente");
+		panel.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("Listar Productos");
+		panel.add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton("Agregar Producto");
+		panel.add(btnNewButton_3);
+		
+		btnNewButton_4 = new JButton("Generar Reporte");
+		panel.add(btnNewButton_4);
+		
+		btnNewButton_5 = new JButton("Configuración");
+		panel.add(btnNewButton_5);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnNewMenu = new JMenu("Mantenimiento");
+		menuBar.add(mnNewMenu);
+		
+		mnNewMenu_1 = new JMenu("Ventas");
+		menuBar.add(mnNewMenu_1);
+		
+		mnNewMenu_2 = new JMenu("Almacen");
+		menuBar.add(mnNewMenu_2);
+		
+		mnNewMenu_3 = new JMenu("Reportes");
+		menuBar.add(mnNewMenu_3);
+		
+		mnNewMenu_4 = new JMenu("Ayuda");
+		menuBar.add(mnNewMenu_4);
 	}
-
+	protected void actionPerformedBtnNewButton(ActionEvent e) {
+	}
 }
