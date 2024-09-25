5cddf7f58d4cd728e3194a276bb56fe610948fad
Jerucho <amadorjeremias2006@gmail.com>
2024-09-24 21:41:09 -0500

    agg: Submenu en el menu item 'mantenimiento'

--- SharpAED/src/clases/Producto.java
+++ SharpAED/src/clases/Producto.java
@@ -1,5 +1,74 @@
 package clases;
 
 public class Producto {
+	private static int codigoProducto = 2001; // Auto-Generado y correlativo a partir de 2001
+	private String nombre;
+	private double precio;
+	private int stockActual;
+	private int stockMinimo;
+	private int stockMaximo;
 
+	
+	public Producto(String nombre, double precio, int stockActual, int stockMinimo, int stockMaximo) {
+		this.codigoProducto++;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stockActual = stockActual;
+		this.stockMinimo = stockMinimo;
+		this.stockMaximo = stockMaximo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+	public int getStockActual() {
+		return stockActual;
+	}
+
+
+	public void setStockActual(int stockActual) {
+		this.stockActual = stockActual;
+	}
+
+
+	public int getStockMinimo() {
+		return stockMinimo;
+	}
+
+
+	public void setStockMinimo(int stockMinimo) {
+		this.stockMinimo = stockMinimo;
+	}
+
+
+	public int getStockMaximo() {
+		return stockMaximo;
+	}
+
+
+	public void setStockMaximo(int stockMaximo) {
+		this.stockMaximo = stockMaximo;
+	}
+	
+	
+	
+	
 }
