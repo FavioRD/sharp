db0e641c094a29d5a69bec153c38aa9b8b525568
Jeremias <jere@amador.com>
2024-10-19 10:56:22 -0500

    arreglo usuarios (terminar)

---
+++ SharpAED/src/arreglos/ArregloUsuarios.java
@@ -0,0 +1,19 @@
+package arreglos;
+
+import java.util.ArrayList;
+
+import clases.Usuario;
+
+public class ArregloUsuarios {
+	ArrayList<Usuario> arregloUsuarios = new ArrayList<Usuario>();
+
+	
+	Usuario admin = new Usuario("admin", "admin");
+	
+	public ArregloUsuarios() {
+		
+	}
+	
+	
+	
+}
