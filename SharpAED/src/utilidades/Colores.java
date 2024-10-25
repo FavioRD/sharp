c796d054fe880f8c5a2305dc3adc7b149c199323
Jerucho <amadorjeremias2006@gmail.com>
2024-10-25 15:02:32 -0500

    agg estilo a la pagina principal

--- SharpAED/src/utilidades/Colores.java
+++ SharpAED/src/utilidades/Colores.java
@@ -4,4 +4,6 @@ import java.awt.Color;
 
 public class Colores {
 	public static final Color ROJO = new Color(255, 94, 98);
+	public static final Color AZUL = new Color(0, 153, 255);
+	public static final Color ROJO_CLARO = new Color(255, 147, 149);
 }
